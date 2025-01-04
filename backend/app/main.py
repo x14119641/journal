b6af7d9bdec53fa4f2516385eb9acffda272392b
x14119641 <daniel.gil.romero24@gmail.com>
2025-01-04 21:01:35 +0100

    Added helloWorld to get message from fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
-
+from fastapi.middleware.cors import CORSMiddleware
 from .schema import Post, PostOut, PostCreate, User, UserCreate, UserResponse
 from .dependencies import db, oauth2_scheme, password_hash
 from typing import List
@@ -9,6 +9,19 @@ from .routes import post, user, auth
 
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 @app.on_event("startup")
 async def startup():
@@ -24,6 +37,10 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(post.router)
 
+
+@app.get("/hello")
+async def root():
+    return {"message": "This is message from FastAPI"}
 
 
 @app.get("/")
