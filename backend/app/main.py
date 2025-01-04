6dd2e38107ea241e69f5c3e55149e2d637c27863
x14119641 <daniel.gil.romero24@gmail.com>
2025-01-04 20:37:09 +0100

    Added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
-
+from fastapi.middleware.cors import CORSMiddleware
 from .schema import Post, PostOut, PostCreate, User, UserCreate, UserResponse
 from .dependencies import db, oauth2_scheme, password_hash
 from typing import List
@@ -9,6 +9,20 @@ from .routes import post, user, auth
 
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 @app.on_event("startup")
 async def startup():
