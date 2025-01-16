d3ab27c5b0d11f782f25b53a7607b3917874e343
x14119641 <daniel.gil.romero24@gmail.com>
2025-01-16 17:08:20 +0100

    Added aliases and refactored the icons in the sideBar

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// Get the directory name using import.meta.url
+const __dirname = new URL('.', import.meta.url).pathname
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': `${__dirname}src`, // Use template literal to build path
+    }
+  }
 })
