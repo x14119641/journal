1e9b918d923007e206b3ed28843693f2894c8204
x14119641 <daniel.gil.romero24@gmail.com>
2025-02-14 18:20:06 +0100

    Refactored profile page

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -2,16 +2,16 @@
   <div class="flex flex-col items-center space-y-6">
     <!-- Top Row: 3 Boxes -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-      <div class="bg-gray-800 rounded-lg shadow-lg">
+      <div class="slate-container">
         <!-- Box 1  -->
         <ProfileInfo />
       </div>
 
-      <div class="bg-gray-800 rounded-lg shadow-lg">
+      <div class="slate-container">
         <!-- Box 2  -->
         <FundsHeader />
       </div>
-      <div class="bg-gray-800 rounded-lg shadow-lg">
+      <div class="slate-container">
         <!-- Box 3  -->
         <PortfolioSummary />
       </div>
@@ -19,11 +19,11 @@
 
     <!-- Bottom Row: 2 Boxes -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 w-full">
-      <div class="bg-gray-800 rounded-lg shadow-lg">
+      <div class="slate-container">
         <!-- Box 4 content goes here -->
         <AllocationDoughnout />
       </div>
-      <div class="bg-gray-800  rounded-lg shadow-lg ">
+      <div class="slate-container">
         <!-- Box 5 content goes here -->
         <PortfolioBarChart />
       </div>
@@ -31,24 +31,9 @@
   </div>
 </template> 
 <script setup lang="ts">
-import ProfileInfo from "../components/ProfileInfo.vue"; // Import the Profile component
+import ProfileInfo from "../components/ProfileInfo.vue"; 
 import FundsHeader from "../components/FundsHeader.vue";
-
-import { onMounted,computed, ref } from 'vue';
 import AllocationDoughnout from "../components/AllocationDoughnout.vue";
-import { usePortfolioStore } from '../stores/portfolioStore';
 import PortfolioBarChart from '../components/PortfolioBarChart.vue';
 import PortfolioSummary from '../components/PortfolioSummary.vue';
-
-
-const portfolioStore = usePortfolioStore();
-
-onMounted(()=>{portfolioStore.getPortfolio()})
-const result = computed(() => portfolioStore.portfolio)
-
-const chartLabels = computed(() => result.value.map(item => item.ticker));
-const chartValues = computed(() => result.value.map(item => item.totalValue));
-// const chartLabels = ref<string[]>(["Stocks", "Bonds", "Real Estate", "Crypto", "Cash"]);
-// const chartValues = ref<number[]>([5000, 3000, 2000, 1500, 1000]);
-const chartColors = ref<string[]>(['#001A6E', '#074799', '#009990', '#E1FFBB', '#536493']);
 </script>
