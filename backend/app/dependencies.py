8b3411eb2979974aa2e52876bc6edfa11cb69ae2
x14119641 <daniel.gil.romero24@gmail.com>
2025-03-01 22:05:46 +0100

    Added a custom erro handler, i might not use it at the end

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,5 +1,8 @@
 # dependencies.py
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import Request
+from fastapi.responses import JSONResponse
+# from ..app.main import app
 from pwdlib import PasswordHash
 from .services.database import Database
 from .config import Settings, Secrets
@@ -27,4 +30,17 @@ def last_day_of_month(any_date):
     # The day 28 exists in every month. 4 days later, it's always next month
     next_month = any_date.replace(day=28) + timedelta(days=4)
     return next_month - timedelta(days=next_month.day)
-    
+
+
+
+class UnicornException(Exception):
+    """Custom Error Handler"""
+    def __init__(self, message: str, status_code:int):
+        self.message = message
+        self.status_code = status_code
+
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=int(exc.status_code),
+        content={"message": exc.message},
+    )
